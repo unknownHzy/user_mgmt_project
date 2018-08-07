4874cd63bfa8381269bd1490594444384d371146
Joy Hu <zhiyuan.hu@nokia.com>
2018-08-07 17:10:52 +0800

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-//若请求是/或者/api 重定向到http://localhost:3000/api/users
+//若请求是/或者/api 重定向到http://localhost:3000/api页面
 router.get(/^(\/|\/api)$/, function(req, res, next) {
   // res.render('index', { title: 'Express' });
     res.cookie('name', 'Joy', {path: '/api', signed: false, maxAge: 600000});
